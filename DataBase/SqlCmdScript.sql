b1edb487cb01b208e1a8bc9ed84b9afdd23bf8b8
young <yh100002@gmail.com>
2019-03-04 22:08:30 +0100

    Deploy User table

--- DataBase/SqlCmdScript.sql
+++ DataBase/SqlCmdScript.sql
@@ -10,3 +10,15 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+CREATE TABLE [dbo].[User](
+	[UserId] [uniqueidentifier] NOT NULL,
+	[UserName] [nvarchar](50) NOT NULL,
+	[PasswordHash] [varbinary](max) NOT NULL,
+	[PasswordSalt] [varbinary](max) NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
